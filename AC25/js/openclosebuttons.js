62c4d2d5d19b401d1cc3a33ae2248005b344d8ec
niels <nielsthl@gmail.com>
2025-01-08 08:58:39 +0100

    AC25

---
+++ AC25/js/openclosebuttons.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".openbs").click(function(){
+	$(".envbuttons").collapse('show');
+    });
+    $(".closebs").click(function(){
+	$(".envbuttons").collapse('hide');
+    });
+
+});
+
+//$(document).ready(function(){
+//    $(".envbuttons").collapse('show');
+//});
